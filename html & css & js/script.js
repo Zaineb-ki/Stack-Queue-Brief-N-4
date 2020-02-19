96275c526060d42d09afd078dc392e3f565de422
taoufiqq <t.elhanchaoui@gmail.com>
2020-02-19 15:05:56 +0100

    finish js page home

--- html & css & js/script.js
+++ html & css & js/script.js
@@ -1,26 +1,22 @@
-function AddPodcast(){
+	function AddPodcast() {
 
-   var b = document.getElementById('podcastTitle').value;
-   var c = document.getElementById('podcaster').value;
-   var d = document.getElementById('categorie').value;
-   var e = document.getElementById('link').value;
-   var f = document.getElementById('tags').value;
-   var g = document.getElementById('description').value;
 
-    document.getElementById("resPodcastTitle").innerHTML += b;
-    document.getElementById("resPodcaster").innerHTML += c;
-    document.getElementById("resCategorie").innerHTML += d;
-    document.getElementById("resLink").innerHTML += e;
-    document.getElementById("resTags").innerHTML += f;
-    document.getElementById("resDescription").innerHTML += g;
+  var all = document.getElementById("container");
 
-  var all = document.getElementById("all");   
-  var z = document.createElement('li');  
-  var x = document.createElement("IMG"); 
-  var y = document.getElementById("adding").value;
-  x.setAttribute("src", y);
-  z.setAttribute("id", "images");
-  all.appendChild(z);
-  z.appendChild(x);
-  document.getElementById("adding").value = '';
+  var img = document.getElementById("url");
+  var podcasT = document.getElementById("podcastTitle");
+  var podcaster = document.getElementById("podcaster");
+  var cat = document.getElementById("categorie");
+  var lien = document.getElementById("link");
+  var tags = document.getElementById("tags");
+  var description = document.getElementById("description");
+  
+
+
+       all.innerHTML += "<div class='div1'>" +"<div>" +"<img width='100%' src =" + img.value  + ">" + "</div>" + "<h1 align='center'>"+ podcasT.value +
+
+        "<h3 align='center'>" + podcaster.value + "</h3>" + "<div>" +  "<p align='center'>" + cat.value + "</p>" + "</div>" + "<h4 align='center'>" + lien.value + "</h4>" + 
+
+        "<h4 align='center'>" + tags.value + "</h4>" + "<h4 align='center'>" + description .value + "</h4>" + "</div>";
+
 }
