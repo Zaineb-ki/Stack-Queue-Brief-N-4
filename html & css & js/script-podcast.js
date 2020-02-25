d41943d944110427a14c827ad22df831f40da952
Zaineb-ki <kossorizaineb@gmail.com>
2020-02-25 14:50:42 +0100

    réglage de css

--- html & css & js/script-podcast.js
+++ html & css & js/script-podcast.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class LinkedList {
     constructor() {
         this.head = null;
@@ -89,26 +88,7 @@ function AddEpisode() {
       "</h4>" + "<h6 class='h6'>" + description.value + "</h6>" + "</div>" +"</span>";
   }
 
-
-{/* <span id="desc">
-                        <p align="center" id="para4">Knicks Chaos, Oscar Picks, Worst 2019 
-                          Movies, and a Claire Danes Interview, With...</p>
-                        <h5>100 minutes · Feb 4</h5>
-
-                        <input type='button' id='button4' value='play' class='myButton'>
-               <div class='display'>
-                        <h4>Episode Info</h4>
-                        <h6 class="h6">HBO and The Ringer’s Bill Simmons is joined by longtime friend and Knicks fan Brian 
-                          Koppelman to discuss the state of the Knicks, Steve Mills’s departure, the possibility of Masai 
-                          Ujiri taking over, and more (2:27). Then Bill sits down with Wesley Morris of The New York Times 
-                          to make Oscar picks and discuss 2019 movies (32:05). Finally, Bill talks with actress Claire Danes 
-                          about acting as a child, some of her past work including ‘My So Called Life,’ ‘Romeo + Juliet,’ and 
-                          ‘Little Women.’ They also discuss the final season of ‘Homeland,’ premiering February 9 (1:04:12).</h6> 
-               </div>
-
-</span> */}
-
-var boutton = document.getElementById('button1');
+  var boutton = document.getElementById('button1');
 var boutton1 = document.getElementById('button2');
 var boutton2 = document.getElementById('button3');
 var boutton3 = document.getElementById('button4');
@@ -150,27 +130,3 @@ boutton3.onclick = function(){
     para2.style.color = "black";
     para3.style.color = "red";
 }
-
-=======
- function AddEpisode() {
-
-
-  var all = document.getElementById("Container");
-
-  var title = document.getElementById("title");
-  var time = document.getElementById("time");
-  var duree = document.getElementById("duree");
-  var description = document.getElementById("description");
-  
-
-
-       all.innerHTML += "<div class='span'>" + "</div>" + "<h1 align='center'>"+ title.value +
-
-        "<h5 align='center'>" + time.value + "</h5>" + "<div>" + 
-
-         "<h5 align='center'>" + duree.value + "</h5>" + "</div>" +
-
-         "<h4 align='center'>" + description .value + "</h4>" + "</div>";
-
-}
->>>>>>> 5fed31c6cbf3e81fc7074aa7c990d1fd77b31a2c
